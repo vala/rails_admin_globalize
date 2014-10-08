e5d1283c61870e31918bdd6147841c88f8aae0ef
zenati <zenati@users.noreply.github.com>
2014-10-08 15:24:00 +0200

    Smaller yet better change

--- lib/rails_admin_globalize.rb
+++ lib/rails_admin_globalize.rb
@@ -54,15 +54,12 @@ module RailsAdmin
             else
               loc = @current_locale = I18n.locale
               I18n.locale = @target_locale = params[:target_locale]
+              param_key = @abstract_model.param_key
 
               satisfy_strong_params!
-              sanitize_params_for!(:update)
+              params[param_key].permit!
 
-              param_key = @abstract_model.param_key
-              translatable_columns = param_key.classify.constantize.translated_attribute_names
-              attributes = params[param_key].extract!(translatable_columns)
-
-              @object.set_attributes(attributes)
+              @object.set_attributes params[param_key]
               @authorization_adapter && @authorization_adapter.attributes_for(:update, @abstract_model).each do |name, value|
                 @object.send("#{name}=", value)
               end
